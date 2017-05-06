ab4b6c3c23e1897216138f5b224f3b361b623bf5
grecian_urn <ljzhu_fdu@163.com>
2017-05-06 13:17:43 +0800

    Test Airplane

--- Classes/MainScene.h
+++ Classes/MainScene.h
@@ -3,6 +3,36 @@
 
 #include "cocos2d.h"
 
+class Airplane : public cocos2d::Sprite
+{
+public:
+	void update();
+	bool isActive() { return active; }
+	bool isSelected() { return selected; }
+	void select() { selected = true; }
+	void activate() { active = true; }
+	void setDest(cocos2d::Point destination) { this->dest = destination; }
+	static Airplane* createPlane(const std::string& filename)
+	{
+		Airplane *airplane = new (std::nothrow) Airplane();
+		if (airplane && airplane->initWithFile(filename))
+		{
+			airplane->autorelease();
+			return airplane;
+		}
+		CC_SAFE_DELETE(airplane);
+		return nullptr;
+	}
+private:
+	bool selected = 0;
+	bool active = 0;
+	int state = 0;
+
+	const float move_speed = 10.0;
+
+	cocos2d::Point dest;
+};
+
 class MainScene : public cocos2d::Layer
 {
 public:
@@ -11,16 +41,16 @@ public:
 	void update(float f) override;
 	bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*)override;
 	void onTouchMoved(cocos2d::Touch*, cocos2d::Event*)override;
-	void onTouchEnded(cocos2d::Touch*, cocos2d::Event*);
+	void onTouchEnded(cocos2d::Touch*, cocos2d::Event*)override;
 
 	CREATE_FUNC(MainScene);
 private:
-	cocos2d::Sprite* airplane1;
-	cocos2d::Sprite* airplane2;
+	std::vector<Airplane*> my_planes;
+	std::vector<Airplane*> enemy_planes;
 	cocos2d::DrawNode* mouse_rect;
 	int state = 0;
 	cocos2d::Vec2 touchPoint{500,500};
-	float moveSpeed = 10.0;
+	
 
 	//EventListenerMouse* _mouseListener;
 };
